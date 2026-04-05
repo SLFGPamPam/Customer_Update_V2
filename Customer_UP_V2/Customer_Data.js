661ba5335b3db53bfb09cd540c7d64354c6d55c2
SLFGPamPam <pampamplayz@gmail.com>
2026-04-05 15:33:27 +0530

    Ty_tobuild portable

---
+++ Customer_UP_V2/Customer_Data.js
@@ -0,0 +1,60 @@
+const CUSTOMER_DATA = {
+  "customers": [
+    {
+      "name": "Customer Name",
+      "points": [
+        {
+          "main": "Main point",
+          "subs": [
+            "Sub point"
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Acme Corporation",
+      "points": [
+        {
+          "main": "Upgraded to premium subscription plan",
+          "subs": [
+            "Annual billing activated",
+            "Priority support enabled"
+          ]
+        },
+        {
+          "main": "Added 5 new team members to account",
+          "subs": []
+        }
+      ]
+    },
+    {
+      "name": "GlobalTech Industries",
+      "points": [
+        {
+          "main": "Requested custom API integration",
+          "subs": [
+            "Documentation sent to development team",
+            "Onboarding call scheduled for next week"
+          ]
+        },
+        {
+          "main": "Payment method updated to corporate card",
+          "subs": []
+        }
+      ]
+    },
+    {
+      "name": "Summit Enterprises",
+      "points": [
+        {
+          "main": "Renewed annual contract early",
+          "subs": [
+            "10% loyalty discount applied",
+            "Extended support hours included"
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Bright Solutions Ltd",
+      "points": [
